629099beb2703b128affc708972581fc10514824
Rachel Barkanic <rachel.barkanic@gmail.com>
2022-09-21 08:51:58 -0700

    need to finish mouseover

--- about.js
+++ about.js
@@ -5,10 +5,24 @@ console.log("hello world");
 function handleSubmit(evt) {
 	evt.preventDefault();
 	
-	console.log('form submit');
+	alert('Your form has been submitted successfully!')
+
 }
+
+
+
+// function mouseOver(event){
+// event.target.alert('hi')
+// }
+
 
 
 let form = document.querySelector('#contact');
 
 form.addEventListener('submit', handleSubmit);
+
+let catCont = document.querySelector('#cat-container');
+
+catCont.addEventListener('mouseover', (event) => {
+event.target.mouseover = alert('meow')
+});
