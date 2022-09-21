ccb3ffaa268ddf26a0ffda823f70c3cb0178334f
Rachel Barkanic <rachel.barkanic@gmail.com>
2022-09-21 12:17:34 -0700

    buttons and alerts added

---
+++ profile.js
@@ -0,0 +1,22 @@
+console.log('meow')
+
+const colorBtn = document.querySelector('#color')
+const placeBtn = document.querySelector('#place')
+const ritualBtn = document.querySelector('#ritual')
+
+
+function favColor() {
+    alert('purple')
+}
+
+function favPlace() {
+    alert('Convit Lake')
+}
+
+function favRitual() {
+    alert('yoga (cliche I know)')
+}
+
+colorBtn.addEventListener('click', favColor);
+placeBtn.addEventListener('click', favPlace);
+ritualBtn.addEventListener('click', favRitual);
