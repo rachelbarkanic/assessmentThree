24a0c5bee28d2b100a61791ef5379b8e7d6bf292
Rachel Barkanic <rachel.barkanic@gmail.com>
2022-09-21 11:44:10 -0700

    added profile js

--- about.js
+++ about.js
@@ -11,18 +11,9 @@ function handleSubmit(evt) {
 
 
 
-// function mouseOver(event){
-// event.target.alert('hi')
-// }
-
-
 
 let form = document.querySelector('#contact');
 
 form.addEventListener('submit', handleSubmit);
 
 let catCont = document.querySelector('#cat-container');
-
-catCont.addEventListener('mouseover', (event) => {
-event.target.mouseover = alert('meow')
-});
